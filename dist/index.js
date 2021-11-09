b52c38fd78c4dc3afae37908a0bcd470d3976345
Estremadoyro <lmestremadoyro@gmail.com>
2021-11-09 10:57:44 -0500

    mikroorm connected, es2019 error wtf

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,18 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@mikro-orm/core");
+const constants_1 = require("./constants");
 const main = async () => {
     const orm = await core_1.MikroORM.init({
-        dbName: 'lireddir',
-        type: 'postgresql',
-        debug: process.env.NODE_ENV !== 'production',
+        entities: ["src/entities/**/*.ts"],
+        dbName: "theposts",
+        type: "postgresql",
+        user: "postgres",
+        password: "postgres",
+        debug: !constants_1.__prod__,
+        clientUrl: "postgresql://postgres:postgres@127.0.0.1:9000",
     });
+    console.log(orm.em);
 };
+main();
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
-import { MikroORM } from '@mikro-orm/core';
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
 
 const main = async () => {
   const orm = await MikroORM.init({
-    dbName: 'lireddir',
-    type: 'postgresql',
-    debug: process.env.NODE_ENV !== 'production',
+    entities: ["src/entities/**/*.ts"],
+    dbName: "theposts",
+    type: "postgresql",
+    user: "postgres",
+    password: "postgres",
+    debug: !__prod__,
+    // Postgres is running at 9000, change to :5432
+    clientUrl: "postgresql://postgres:postgres@127.0.0.1:9000",
   });
+  console.log(orm.em);
 };
+
+main();
