3969255f97beb02ce9c05dc9626bf22ffb1e23d7
Leonardo <leonardo@mac.local>
2021-12-21 23:29:29 -0500

    gitignore

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
-import { MikroORM } from "@mikro-orm/core";
-import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
+import { MikroORM } from '@mikro-orm/core';
+import { __prod__ } from './constants';
+import { Post } from './entities/Post';
 
 const main = async () => {
   const orm = await MikroORM.init({
     // It won't find it in the JS compiled file
     // entities: ["src/entities/**/*.ts"],
     entities: [Post],
-    dbName: "theposts",
-    type: "postgresql",
-    user: "postgres",
-    password: "postgres",
+    dbName: 'theposts',
+    type: 'postgresql',
+    user: 'postgres',
+    password: 'postgres',
     debug: !__prod__,
     // Postgres is running at 9000, change to :5432
-    clientUrl: "postgresql://postgres:postgres@127.0.0.1:9000",
+    clientUrl: 'postgresql://postgres:postgres@127.0.0.1:9000',
   });
   console.log(orm.em);
 };
